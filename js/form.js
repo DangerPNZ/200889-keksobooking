aaa196b03198dfbd6fcb41c3de13b6bc504d5524
DangerPNZ <dangerpnz@mail.ru>
2017-02-02 21:00:07 +0400

    Правленные ошибки

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 'use strict';
 // Переменные для указателей
 var pin = document.querySelectorAll('.pin');
-var currentActive = document.querySelector('.pin--active');
 
 // Переменные для диалогового окна
 var dialog = document.querySelector('.dialog');
@@ -9,7 +8,6 @@ var dialogClose = document.querySelector('.dialog__close');
 
 // Переменные для валидации полей формы
 var title = document.querySelector('#title');
-var price = document.querySelector('#price');
 var address = document.querySelector('#address');
 
 // Переменные для синхронизации полей формы
@@ -20,25 +18,27 @@ var price = document.querySelector('#price');
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
 
+var disableActive = function () {
+  var element = document.querySelector('.pin--active');
+  if (element) {
+    element.classList.remove('pin--active');
+  }
+};
+
 // цикл для переключения активного указателя
 for (var i = 0; i < pin.length; i++) {
-  (function (n) {
-    pin[n].addEventListener('click', function () {
-      currentActive.classList.remove('pin--active');
-      currentActive = pin[n];
-      pin[n].classList.add('pin--active');
-      // уточнить корректность решения задачи
-      dialog.style.display = 'block';
-    });
-  })(i);
+  pin[i].addEventListener('click', function (e) {
+    disableActive();
+    e.currentTarget.classList.add('pin--active');
+    dialog.classList.remove('invisible');
+  });
 }
 
 // закрытие диалогового окна и удаление активного указателя
 dialogClose.addEventListener('click', function (event) {
-  // уточнить корректность решения задачи
   event.preventDefault();
-  currentActive.classList.remove('pin--active');
-  dialog.style.display = 'none';
+  disableActive();
+  dialog.classList.add('invisible');
 });
 
 // Валидация полей формы
@@ -63,22 +63,14 @@ timeoutOption.addEventListener('change', function (event) {
   timeOption.value = event.target.value;
 });
 
-type.addEventListener('change', function (event) {
-  if (price.value === 0) {
-    event.target.value = '2';
-  } else
-  if (price.value >= 1000) {
-    event.target.value = '1';
-  } else
-  if (price.value >= 10000) {
-    event.target.value = '3';
-  }
-});
-
 roomNumber.addEventListener('change', function (event) {
   capacity.value = event.target.value;
 });
 
 capacity.addEventListener('change', function (event) {
   roomNumber.value = event.target.value;
+});
+
+type.addEventListener('change', function (event) {
+  price.value = event.target.value;
 });
