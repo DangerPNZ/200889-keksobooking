e86db32b9e3e328aac57b8e38df5e79a1fc0d216
DangerPNZ <dangerpnz@mail.ru>
2017-02-06 17:17:36 +0400

    Выполнил задание

--- js/form.js
+++ js/form.js
@@ -1,4 +1,6 @@
 'use strict';
+
+var tokyoMap = document.querySelector('.tokyo__pin-map');
 // Переменные для указателей
 var pin = document.querySelectorAll('.pin');
 
@@ -8,37 +10,124 @@ var dialogClose = document.querySelector('.dialog__close');
 
 // Переменные для валидации полей формы
 var title = document.querySelector('#title');
+var price = document.querySelector('#price');
 var address = document.querySelector('#address');
 
 // Переменные для синхронизации полей формы
 var timeOption = document.querySelector('#time');
 var timeoutOption = document.querySelector('#timeout');
 var type = document.querySelector('#type');
-var price = document.querySelector('#price');
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
 
-var disableActive = function () {
+// Константы для keyCode
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
+// DRY или определение функций для дублируемого кода
+
+// Функции для проверки соответствия keyCode
+var deactivatingEvent = function (event) {
+  return event.keyCode && event.keyCode === ESCAPE_KEY_CODE;
+};
+var activatingEvent = function (event) {
+  return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+};
+// функция, находящая элемент с классом pin--active, и, если элемент с таким классом существует, удаляет этот класс у найденого элемента
+var disableActivePin = function () {
   var element = document.querySelector('.pin--active');
   if (element) {
     element.classList.remove('pin--active');
   }
 };
 
-// цикл для переключения активного указателя
-for (var i = 0; i < pin.length; i++) {
-  pin[i].addEventListener('click', function (e) {
-    disableActive();
-    e.currentTarget.classList.add('pin--active');
-    dialog.classList.remove('invisible');
+// функция для определения активности pin и присвоения соответствующего статуса aria-pressed для каждого указателя
+var determineAriaPressed = function (element) {
+  if (element.classList.contains('pin--active')) {
+    element.setAttribute('aria-pressed', true);
+  } else {
+    element.setAttribute('aria-pressed', false);
+  }
+};
+
+// aункция для управления отображения показа диалогового окна
+var setDialogClassInvisible = function (action) {
+  dialog.classList[action]('invisible');
+};
+
+// функция для переключения aria-pressed в завсимости от активности указателя pin
+var toggleAriaPressed = function () {
+  for (var i = 0; i < pin.length; i++) {
+    determineAriaPressed(pin[i]);
+  }
+};
+
+// Вызываем функцию для определения наличия активного указателя на странице и присвоения каждому соответствующего статуса aria-pressed
+toggleAriaPressed();
+
+// закрытие диалогового окна и удаление активного указателя
+var deactivateDialogAndPin = function () {
+  event.preventDefault();
+  disableActivePin();
+  setDialogClassInvisible('add');
+  // динамическое изменение статуса активного (нажатого) указателя (aria-pressed) при удалении всех активных указателей
+  toggleAriaPressed();
+};
+
+var activatePin = function () {
+  if (event.target.className === 'pin') {
+    disableActivePin();
+    event.target.classList.add('pin--active');
+    setDialogClassInvisible('remove');
+    // динамическое переключение статуса (aria-pressed) при переключении активного указателя
+    toggleAriaPressed();
+  }
+};
+
+/* если у модального окна отсутствует класс invisible (соответственно, оно открыто),
+ реализовываем его закрытие по нажатию esc, при этом диалоговое окно может быть не в фокусе
+ вместе с этим происходит деактивация активного указателя и переопределение его статуса aria-pressed на false */
+if (!document.querySelector('.invisible')) {
+  document.addEventListener('keydown', function (event) {
+    if (deactivatingEvent(event)) {
+      disableActivePin();
+      setDialogClassInvisible('add');
+      // динамическое изменение статуса активного (нажатого) указателя (aria-pressed) при удалении всех активных указателей
+      toggleAriaPressed();
+    }
   });
 }
 
-// закрытие диалогового окна и удаление активного указателя
+// Активизация активного указателя по клику
+tokyoMap.addEventListener('click', function (event) {
+  if (event.target.tagName === 'IMG') {
+    disableActivePin();
+    event.path[1].classList.add('pin--active');
+    setDialogClassInvisible('remove');
+    // динамическое переключение статуса (aria-pressed) при переключении активного указателя
+    toggleAriaPressed();
+  }
+  activatePin();
+});
+
+// Активизация активного указателя по нажатию клавиши enter
+tokyoMap.addEventListener('keydown', function (event) {
+  if (activatingEvent(event)) {
+    activatePin();
+  }
+});
+
+
+// закрытие диалогового окна и удаление активного указателя по клику
 dialogClose.addEventListener('click', function (event) {
-  event.preventDefault();
-  disableActive();
-  dialog.classList.add('invisible');
+  deactivateDialogAndPin();
+});
+
+// Закрытие диалогового окна и снятие активного указателя по нажатию escape
+dialogClose.addEventListener('keydown', function (event) {
+  if (activatingEvent(event)) {
+    deactivateDialogAndPin();
+  }
 });
 
 // Валидация полей формы
