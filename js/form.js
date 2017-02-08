b7c410d0f673b2b5102a27bd407dafc2d6613b72
DangerPNZ <dangerpnz@mail.ru>
2017-02-08 20:37:44 +0400

    initialize-pins(первый этап задания, на проверку(повторный коммит)

--- js/form.js
+++ js/form.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// Переменные для синхронизации полей формы
-var timeOption = document.querySelector('#time');
-var timeoutOption = document.querySelector('#timeout');
-var type = document.querySelector('#type');
-var roomNumber = document.querySelector('#room_number');
-var capacity = document.querySelector('#capacity');
-
 // Переменные для валидации полей формы
 var title = document.querySelector('#title');
 var price = document.querySelector('#price');
@@ -25,24 +18,3 @@ price.min = '1000';
 price.max = '1000000';
 
 address.required = true;
-
-// Синхронизация полей форм
-timeOption.addEventListener('change', function (e) {
-  timeoutOption.value = e.target.value;
-});
-
-timeoutOption.addEventListener('change', function (e) {
-  timeOption.value = e.target.value;
-});
-
-roomNumber.addEventListener('change', function (e) {
-  capacity.value = e.target.value;
-});
-
-capacity.addEventListener('change', function (e) {
-  roomNumber.value = e.target.value;
-});
-
-type.addEventListener('change', function (e) {
-  price.value = e.target.value;
-});
