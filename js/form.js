f7595d49c4e40076ef543af8e767ecdfdc2d4df7
DangerPNZ <dangerpnz@mail.ru>
2017-02-16 01:14:22 +0400

    Выполнил инкапсуляцию модулей с помощью IIFE функций, исправил баг с пинами, использовав closest

--- js/form.js
+++ js/form.js
@@ -1,37 +1,39 @@
 'use strict';
 
+(function () {
 // Переменные для валидации полей формы
-var title = document.querySelector('#title');
-var price = document.querySelector('#price');
-var address = document.querySelector('#address');
+  var title = document.querySelector('#title');
+  var price = document.querySelector('#price');
+  var address = document.querySelector('#address');
 
 // Переменные для синхронизации полей формы
-var timeOption = document.querySelector('#time');
-var timeoutOption = document.querySelector('#timeout');
-var type = document.querySelector('#type');
-var roomNumber = document.querySelector('#room_number');
-var capacity = document.querySelector('#capacity');
+  var timeOption = document.querySelector('#time');
+  var timeoutOption = document.querySelector('#timeout');
+  var type = document.querySelector('#type');
+  var roomNumber = document.querySelector('#room_number');
+  var capacity = document.querySelector('#capacity');
 
 // Вызываем функцию управления указателями pin и всеми связанными с неми операциями
-window.initializePins();
+  window.initializePins();
 
 // Валидация полей формы
-title.required = true;
-title.minLength = '30';
-title.maxLength = '100';
+  title.required = true;
+  title.minLength = '30';
+  title.maxLength = '100';
 
-window.price.required = true;
-window.price.type = 'number';
-window.price.min = '1000';
-window.price.max = '1000000';
+  window.price.required = true;
+  window.price.type = 'number';
+  window.price.min = '1000';
+  window.price.max = '1000000';
 
-address.required = true;
+  address.required = true;
 
 // Синхронизация полей форм
-window.synchronizeFields(timeOption, timeoutOption, ['12', '13', '14'], ['12', '13', '14'], 'value');
+  window.synchronizeFields(timeOption, timeoutOption, ['12', '13', '14'], ['12', '13', '14'], 'value');
 
-window.synchronizeFields(roomNumber, capacity, ['1', '2', '2'], ['1', '2', '2'], 'value');
+  window.synchronizeFields(roomNumber, capacity, ['1', '2', '2'], ['1', '2', '2'], 'value');
 
-window.synchronizeFields(type, price, ['1000', '0', '10000'], ['1000', '0', '10000'], 'min');
+  window.synchronizeFields(type, price, ['1000', '0', '10000'], ['1000', '0', '10000'], 'min');
 
-window.synchronizeFields(type, price, ['1000', '0', '10000'], ['1000', '0', '10000'], 'value');
+  window.synchronizeFields(type, price, ['1000', '0', '10000'], ['1000', '0', '10000'], 'value');
+})();
--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,136 +1,123 @@
 'use strict';
 
-window.initializePins = function () {
+(function () {
+  window.initializePins = function () {
+    var tokyoMap = document.querySelector('.tokyo__pin-map'); /* Переменная для блока, содержащего указатели */
+    var pin = document.querySelectorAll('.pin'); // Переменная для указателей
+
+// Переменные для диалогового окна
+    var dialog = document.querySelector('.dialog'); // Модальное окно
+    var dialogClose = document.querySelector('.dialog__close'); /* Кнопка закрыть модальное окно */
+
+// Константы для keyCode
+    var ENTER_KEY_CODE = 13;
+    var ESCAPE_KEY_CODE = 27;
 
 // DRY или определение функций для дублируемого кода
 
 // Функции для проверки соответствия keyCode
-  var activatingEvent = (function () {
-    var ENTER_KEY_CODE = 13;
-    return function (e) {
-      return e.keyCode && e.keyCode === ENTER_KEY_CODE;
-    };
-  })();
-
-  var deactivatingEvent = (function () {
-    var ESCAPE_KEY_CODE = 27;
-    return function (e) {
+    var deactivatingEvent = function (e) {
       return e.keyCode && e.keyCode === ESCAPE_KEY_CODE;
     };
-  })();
+    var activatingEvent = function (e) {
+      return e.keyCode && e.keyCode === ENTER_KEY_CODE;
+    };
 
 // функция, находящая элемент с классом pin--active, и, если элемент с таким классом существует, удаляет этот класс у найденого элемента
-  var disableActivePin = function () {
-    var element = document.querySelector('.pin--active');
-    if (element) {
-      element.classList.remove('pin--active');
-    }
-  };
+    var disableActivePin = function () {
+      var element = document.querySelector('.pin--active');
+      if (element) {
+        element.classList.remove('pin--active');
+      }
+    };
 
 // функция для определения активности pin и присвоения соответствующего статуса aria-pressed для каждого указателя
-  var determineAriaPressed = (function () {
-    return function (element) {
+    var determineAriaPressed = function (element) {
       if (element.classList.contains('pin--active')) {
         element.setAttribute('aria-pressed', true);
       } else {
         element.setAttribute('aria-pressed', false);
       }
     };
-  })();
 
 // функция для переключения aria-pressed в завсимости от активности указателя pin
-  var toggleAriaPressed = (function () {
-    var pin = document.querySelectorAll('.pin'); // Переменная для указателей
-    return function () {
+    var toggleAriaPressed = function () {
       for (var i = 0; i < pin.length; i++) {
         determineAriaPressed(pin[i]);
       }
     };
-  })();
 
 // функция для управления отображением диалогового окна
-  var setDialogClassInvisible = (function () {
-    var dialog = document.querySelector('.dialog'); // Модальное окно
-    return function (action) {
+    var setDialogClassInvisible = function (action) {
       dialog.classList[action]('invisible');
     };
-  })();
 
 // закрытие диалогового окна и удаление активного указателя
-  var deactivateDialogAndPin = (function () {
-    return function () {
+    var deactivateDialogAndPin = function () {
       disableActivePin();
       setDialogClassInvisible('add');
   // динамическое изменение статуса активного (нажатого) указателя (aria-pressed) при удалении всех активных указателей
       toggleAriaPressed();
     };
-  })();
 
 // функция для активации выбранного указателя, деактивации ранее выбранного.
 // Также открывает модальное окно и переключает значение аттрибута aria-pressed.
-  var activatePin = (function () {
-    return function (e) {
-      if (e.target.className === 'pin' || 'pin  pin__main') {
-        disableActivePin();
-        e.target.classList.add('pin--active');
-        setDialogClassInvisible('remove');
-      // динамическое переключение статуса (aria-pressed) при переключении активного указателя
-        toggleAriaPressed();
-      }
+    var activatePin = function (e) {
+      disableActivePin();
+      e.target.closest('.pin').classList.add('pin--active');
+      setDialogClassInvisible('remove');
+    // динамическое переключение статуса (aria-pressed) при переключении активного указателя
+      toggleAriaPressed();
     };
-  })();
+
 
 /* АЛГОРИТМ работы функции */
   // Вызываем функцию для определения наличия активного указателя на странице и присвоения каждому соответствующего статуса aria-pressed
-  toggleAriaPressed();
+    toggleAriaPressed();
   /* ЕСЛИ на странице есть элемент с классом pin--active (соответственно, имеется активный указатель pin),
    то и модальное окно dialog тоже открыто.
    Реализовываем его закрытие по нажатию esc, при этом диалоговое окно может быть не в фокусе.
    Вместе с этим происходит деактивация активного указателя и переопределение его статуса aria-pressed на false */
-  if (document.querySelector('.pin--active')) {
-    document.addEventListener('keydown', function (e) {
-      if (deactivatingEvent(e)) {
-        disableActivePin();
-        setDialogClassInvisible('add');
+    if (document.querySelector('.pin--active')) {
+      document.addEventListener('keydown', function (e) {
+        if (deactivatingEvent(e)) {
+          disableActivePin();
+          setDialogClassInvisible('add');
         // динамическое изменение статуса активного (нажатого) указателя (aria-pressed) при удалении всех активных указателей
-        toggleAriaPressed();
-      }
-    });
-  }
+          toggleAriaPressed();
+        }
+      });
+    }
   // Активизация активного указателя по клику
-  (function () {
-    var tokyoMap = document.querySelector('.tokyo__pin-map'); /* Переменная для блока, содержащего указатели */
     tokyoMap.addEventListener('click', function (e) {
       if (e.target.className === 'rounded') {
         disableActivePin();
         e.target.parentNode.classList.add('pin--active');
         setDialogClassInvisible('remove');
-        // динамическое переключение статуса (aria-pressed) при переключении активного указателя
+      // динамическое переключение статуса (aria-pressed) при переключении активного указателя
         toggleAriaPressed();
       }
       activatePin(e);
     });
 
-    // Активизация активного указателя по нажатию клавиши enter
+  // Активизация активного указателя по нажатию клавиши enter
     tokyoMap.addEventListener('keydown', function (e) {
       if (activatingEvent(e)) {
         activatePin(e);
       }
     });
-  })();
 
-  (function () {
-    var dialogClose = document.querySelector('.dialog__close'); /* Кнопка закрыть модальное окно */
-    // закрытие диалогового окна и удаление активного указателя по клику
+
+  // закрытие диалогового окна и удаление активного указателя по клику
     dialogClose.addEventListener('click', function (e) {
       deactivateDialogAndPin();
     });
 
-    // Закрытие диалогового окна и снятие активного указателя по нажатию escape
+  // Закрытие диалогового окна и снятие активного указателя по нажатию escape
     dialogClose.addEventListener('keydown', function (e) {
       if (activatingEvent(e)) {
         deactivateDialogAndPin();
       }
     });
-  })();
-};
+  };
+})();
