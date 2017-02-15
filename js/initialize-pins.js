7baaf9b393c440040424c267fb46f007d07bc74e
DangerPNZ <dangerpnz@mail.ru>
2017-02-15 17:29:53 +0400

    Изоляция, файл initialize-pins

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,26 +1,23 @@
 'use strict';
 
 window.initializePins = function () {
-  var tokyoMap = document.querySelector('.tokyo__pin-map'); /* Переменная для блока, содержащего указатели */
-  var pin = document.querySelectorAll('.pin'); // Переменная для указателей
-
-// Переменные для диалогового окна
-  var dialog = document.querySelector('.dialog'); // Модальное окно
-  var dialogClose = document.querySelector('.dialog__close'); /* Кнопка закрыть модальное окно */
-
-// Константы для keyCode
-  var ENTER_KEY_CODE = 13;
-  var ESCAPE_KEY_CODE = 27;
 
 // DRY или определение функций для дублируемого кода
 
 // Функции для проверки соответствия keyCode
-  var deactivatingEvent = function (e) {
-    return e.keyCode && e.keyCode === ESCAPE_KEY_CODE;
-  };
-  var activatingEvent = function (e) {
-    return e.keyCode && e.keyCode === ENTER_KEY_CODE;
-  };
+  var activatingEvent = (function () {
+    var ENTER_KEY_CODE = 13;
+    return function (e) {
+      return e.keyCode && e.keyCode === ENTER_KEY_CODE;
+    };
+  })();
+
+  var deactivatingEvent = (function () {
+    var ESCAPE_KEY_CODE = 27;
+    return function (e) {
+      return e.keyCode && e.keyCode === ESCAPE_KEY_CODE;
+    };
+  })();
 
 // функция, находящая элемент с классом pin--active, и, если элемент с таким классом существует, удаляет этот класс у найденого элемента
   var disableActivePin = function () {
@@ -31,46 +28,57 @@ window.initializePins = function () {
   };
 
 // функция для определения активности pin и присвоения соответствующего статуса aria-pressed для каждого указателя
-  var determineAriaPressed = function (element) {
-    if (element.classList.contains('pin--active')) {
-      element.setAttribute('aria-pressed', true);
-    } else {
-      element.setAttribute('aria-pressed', false);
-    }
-  };
+  var determineAriaPressed = (function () {
+    return function (element) {
+      if (element.classList.contains('pin--active')) {
+        element.setAttribute('aria-pressed', true);
+      } else {
+        element.setAttribute('aria-pressed', false);
+      }
+    };
+  })();
 
 // функция для переключения aria-pressed в завсимости от активности указателя pin
-  var toggleAriaPressed = function () {
-    for (var i = 0; i < pin.length; i++) {
-      determineAriaPressed(pin[i]);
-    }
-  };
+  var toggleAriaPressed = (function () {
+    var pin = document.querySelectorAll('.pin'); // Переменная для указателей
+    return function () {
+      for (var i = 0; i < pin.length; i++) {
+        determineAriaPressed(pin[i]);
+      }
+    };
+  })();
 
 // функция для управления отображением диалогового окна
-  var setDialogClassInvisible = function (action) {
-    dialog.classList[action]('invisible');
-  };
+  var setDialogClassInvisible = (function () {
+    var dialog = document.querySelector('.dialog'); // Модальное окно
+    return function (action) {
+      dialog.classList[action]('invisible');
+    };
+  })();
 
 // закрытие диалогового окна и удаление активного указателя
-  var deactivateDialogAndPin = function () {
-    disableActivePin();
-    setDialogClassInvisible('add');
+  var deactivateDialogAndPin = (function () {
+    return function () {
+      disableActivePin();
+      setDialogClassInvisible('add');
   // динамическое изменение статуса активного (нажатого) указателя (aria-pressed) при удалении всех активных указателей
-    toggleAriaPressed();
-  };
+      toggleAriaPressed();
+    };
+  })();
 
 // функция для активации выбранного указателя, деактивации ранее выбранного.
 // Также открывает модальное окно и переключает значение аттрибута aria-pressed.
-  var activatePin = function (e) {
-    if (e.target.className === 'pin') {
-      disableActivePin();
-      e.target.classList.add('pin--active');
-      setDialogClassInvisible('remove');
-    // динамическое переключение статуса (aria-pressed) при переключении активного указателя
-      toggleAriaPressed();
-    }
-  };
-
+  var activatePin = (function () {
+    return function (e) {
+      if (e.target.className === 'pin') {
+        disableActivePin();
+        e.target.classList.add('pin--active');
+        setDialogClassInvisible('remove');
+      // динамическое переключение статуса (aria-pressed) при переключении активного указателя
+        toggleAriaPressed();
+      }
+    };
+  })();
 
 /* АЛГОРИТМ работы функции */
   // Вызываем функцию для определения наличия активного указателя на странице и присвоения каждому соответствующего статуса aria-pressed
@@ -90,34 +98,39 @@ window.initializePins = function () {
     });
   }
   // Активизация активного указателя по клику
-  tokyoMap.addEventListener('click', function (e) {
-    if (e.target.className === 'rounded') {
-      disableActivePin();
-      e.target.parentNode.classList.add('pin--active');
-      setDialogClassInvisible('remove');
-      // динамическое переключение статуса (aria-pressed) при переключении активного указателя
-      toggleAriaPressed();
-    }
-    activatePin(e);
-  });
-
-  // Активизация активного указателя по нажатию клавиши enter
-  tokyoMap.addEventListener('keydown', function (e) {
-    if (activatingEvent(e)) {
+  (function () {
+    var tokyoMap = document.querySelector('.tokyo__pin-map'); /* Переменная для блока, содержащего указатели */
+    tokyoMap.addEventListener('click', function (e) {
+      if (e.target.className === 'rounded') {
+        disableActivePin();
+        e.target.parentNode.classList.add('pin--active');
+        setDialogClassInvisible('remove');
+        // динамическое переключение статуса (aria-pressed) при переключении активного указателя
+        toggleAriaPressed();
+      }
       activatePin(e);
-    }
-  });
+    });
 
+    // Активизация активного указателя по нажатию клавиши enter
+    tokyoMap.addEventListener('keydown', function (e) {
+      if (activatingEvent(e)) {
+        activatePin(e);
+      }
+    });
+  })();
 
-  // закрытие диалогового окна и удаление активного указателя по клику
-  dialogClose.addEventListener('click', function (e) {
-    deactivateDialogAndPin();
-  });
-
-  // Закрытие диалогового окна и снятие активного указателя по нажатию escape
-  dialogClose.addEventListener('keydown', function (e) {
-    if (activatingEvent(e)) {
+  (function () {
+    var dialogClose = document.querySelector('.dialog__close'); /* Кнопка закрыть модальное окно */
+    // закрытие диалогового окна и удаление активного указателя по клику
+    dialogClose.addEventListener('click', function (e) {
       deactivateDialogAndPin();
-    }
-  });
+    });
+
+    // Закрытие диалогового окна и снятие активного указателя по нажатию escape
+    dialogClose.addEventListener('keydown', function (e) {
+      if (activatingEvent(e)) {
+        deactivateDialogAndPin();
+      }
+    });
+  })();
 };
