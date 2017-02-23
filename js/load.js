007bb8456d8f5b16711d242f796e6b6e7b032e0c
DangerPNZ <dangerpnz@mail.ru>
2017-02-23 10:23:23 +0400

    модуль load

--- js/load.js
+++ js/load.js
@@ -1,16 +1,14 @@
 'use strict';
 
-window.load = (function (url, onLoad) {
-  var xhr = new XMLHttpRequest();
-  var similarApartments = [];
-  onLoad = function (findings) {
-    document.write(JSON.stringefy(similarApartments));
+window.load = (function () {
+  return function (url, onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function (e) {
+      if (e.target.status === 200) {
+        onLoad(JSON.parse(e.target.response));
+      }
+    });
+    xhr.send();
   };
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function (e) {
-    if (e.target.status === 200) {
-      onLoad('JSON.parse(e.target.response)');
-    }
-  });
-  xhr.send();
 })();
